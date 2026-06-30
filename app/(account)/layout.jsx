21ee4cd31330053cf0fddbb69a6b0a52caff9410
Noyon Sarker <noyon@ba-systems.com>
2026-06-30 10:32:45 +0600

    done

---
+++ app/(account)/layout.jsx
@@ -0,0 +1,15 @@
+import PublicLayout from "@/components/layouts/public/PublicLayout";
+import AccountGuard from "@/components/providers/AccountGuard";
+
+export const metadata = {
+  title: "My Account - EcommerceStore",
+  description: "Manage your account, orders, and more.",
+};
+
+export default function AccountGroupLayout({ children }) {
+  return (
+    <PublicLayout>
+      <AccountGuard>{children}</AccountGuard>
+    </PublicLayout>
+  );
+}
